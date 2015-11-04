89dd7648e7002c83a72f62bc2f91c64d9ae45db6
Junghoon, Park <jh9216.park@samsung.com>
2015-11-04 16:16:03 +0900

    initial release
    Change-Id: I3a001fa3c414abbaea717ee38af0119c06b32ce6
    Signed-off-by: Junghoon, Park <jh9216.park@samsung.com>

---
+++ inc/key.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __KEY_H__
+#define __KEY_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define AUL_K_STARTTIME     "__AUL_STARTTIME__"
+#define AUL_K_EXEC      "__AUL_EXEC__"
+#define AUL_K_PACKAGETYPE   "__AUL_PACKAGETYPE__"
+#define AUL_K_HWACC     "__AUL_HWACC__"
+#define AUL_K_APPID     "__AUL_APPID__"
+#define AUL_K_PID       "__AUL_PID__"
+#define AUL_K_TASKMANAGE    "__AUL_TASKMANAGE__"
+#define AUL_K_INTERNAL_POOL "__AUL_INTERNAL_POOL__"
+#define AUL_K_PKGID     "__AUL_PKGID_"
+#define AUL_K_DEBUG	"__AUL_DEBUG__"
+#define AUL_K_PRIVACY_APPID		"__AUL_PRIVACY_APPID__"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* __KEY_H__ */
+
